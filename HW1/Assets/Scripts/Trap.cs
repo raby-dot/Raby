efb561bea4e259a74363cdb9300c4b852642e34e
raby-dot <rabiakursun1109@gmail.com>
2026-03-18 14:04:09 +0300

    Add files via upload

---
+++ HW1/Assets/Scripts/Trap.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    public float activeDuration = 2f;
+    public float inactiveDuration = 2f;
+    private MeshRenderer[] allRenderers;
+    private Collider[] allColliders;
+
+    void Awake()
+    {
+   
+        allRenderers = GetComponentsInChildren<MeshRenderer>();
+        allColliders = GetComponentsInChildren<Collider>();
+    }
+
+    void Start()
+    {
+        StartCoroutine(TrapCycle());
+    }
+
+    IEnumerator TrapCycle()
+    {
+        while (true)
+        {
+            SetTrapState(false);
+            yield return new WaitForSeconds(inactiveDuration);
+
+            SetTrapState(true);
+            yield return new WaitForSeconds(activeDuration);
+        }
+    }
+
+    void SetTrapState(bool state)
+    {
+        foreach (var r in allRenderers) r.enabled = state;
+        foreach (var c in allColliders) c.enabled = state;
+    }
+  
+    private void OnCollisionEnter(Collision collision)
+    {
+    
+        if (collision.gameObject.CompareTag("Player"))
+        {
+      
+            collision.gameObject.GetComponent<PlayerController>().Die();
+        }
+    }
+}
